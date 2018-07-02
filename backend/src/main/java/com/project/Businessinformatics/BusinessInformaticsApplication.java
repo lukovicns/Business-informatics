3c4533328ad91ff0c9c8e8582e076aa4c4fd5b72
Milos <lukovicns@uns.ac.rs>
2018-07-02 03:08:57 +0200

    commit

---
+++ backend/src/main/java/com/project/Businessinformatics/BusinessInformaticsApplication.java
@@ -0,0 +1,12 @@
+package com.project.Businessinformatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessInformaticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessInformaticsApplication.class, args);
+	}
+}
